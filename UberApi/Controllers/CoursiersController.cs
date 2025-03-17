432ef6427059ac067386386b144b504b591afc24
bekhoucm <bekhoucm@D250-S30>
2025-03-17 11:43:35 +0100

    pas finin tkt

--- UberApi/Controllers/CoursiersController.cs
+++ UberApi/Controllers/CoursiersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UberApi.Models.EntityFramework;
+using UberApi.Models.Repository;
 
 namespace UberApi.Controllers
 {
@@ -13,25 +14,25 @@ namespace UberApi.Controllers
     [ApiController]
     public class CoursiersController : ControllerBase
     {
-        private readonly S221UberContext _context;
+        private readonly IDataRepository<Coursier> dataRepository;
 
-        public CoursiersController(S221UberContext context)
+        public CoursiersController(IDataRepository<Coursier> dataRepo)
         {
-            _context = context;
+            dataRepository = dataRepo;
         }
 
         // GET: api/Coursiers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Coursier>>> GetCoursiers()
         {
-            return await _context.Coursiers.ToListAsync();
+            return dataRepository.GetAll();
         }
 
         // GET: api/Coursiers/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Coursier>> GetCoursier(int id)
         {
-            var coursier = await _context.Coursiers.FindAsync(id);
+            var coursier = dataRepository.GetById(id);
 
             if (coursier == null)
             {
@@ -43,65 +44,65 @@ namespace UberApi.Controllers
 
         // PUT: api/Coursiers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCoursier(int id, Coursier coursier)
-        {
-            if (id != coursier.IdCoursier)
-            {
-                return BadRequest();
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutCoursier(int id, Coursier coursier)
+        //{
+        //    if (id != coursier.IdCoursier)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(coursier).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CoursierExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return NoContent();
-        }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CoursierExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-        // POST: api/Coursiers
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Coursier>> PostCoursier(Coursier coursier)
-        {
-            _context.Coursiers.Add(coursier);
-            await _context.SaveChangesAsync();
+        //    return NoContent();
+        //}
 
-            return CreatedAtAction("GetCoursier", new { id = coursier.IdCoursier }, coursier);
-        }
+        //POST: api/Coursiers
+        //To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Coursier>> PostCoursier(Coursier coursier)
+        //{
+        //    _context.Coursiers.Add(coursier);
+        //    await _context.SaveChangesAsync();
 
-        // DELETE: api/Coursiers/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCoursier(int id)
-        {
-            var coursier = await _context.Coursiers.FindAsync(id);
-            if (coursier == null)
-            {
-                return NotFound();
-            }
+        //    return CreatedAtAction("GetCoursier", new { id = coursier.IdCoursier }, coursier);
+        //}
 
-            _context.Coursiers.Remove(coursier);
-            await _context.SaveChangesAsync();
+        //DELETE: api/Coursiers/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteCoursier(int id)
+        //{
+        //    var coursier = await _context.Coursiers.FindAsync(id);
+        //    if (coursier == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return NoContent();
-        }
+        //    _context.Coursiers.Remove(coursier);
+        //    await _context.SaveChangesAsync();
 
-        private bool CoursierExists(int id)
-        {
-            return _context.Coursiers.Any(e => e.IdCoursier == id);
-        }
+        //    return NoContent();
+        //}
+
+        //private bool CoursierExists(int id)
+        //{
+        //    return _context.Coursiers.Any(e => e.IdCoursier == id);
+        //}
     }
 }
---
+++ UberApi/Models/DataManager/CoursierManager.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UberApi.Models.EntityFramework;
+using UberApi.Models.Repository;
+
+namespace UberApi.Models.DataManager
+{
+    public class CoursierManager : IDataRepository<Coursier>
+    {
+        readonly S221UberContext? s221UberContext;
+        public CoursierManager() { }
+        public CoursierManager(S221UberContext context)
+        {
+            s221UberContext = context;
+        }
+        public ActionResult<IEnumerable<Coursier>> GetAll()
+        {
+            return s221UberContext.Coursiers.ToList();
+        }
+        public ActionResult<Coursier> GetById(int id)
+        {
+            return s221UberContext.Coursiers.FirstOrDefault(u => u.IdCoursier == id);
+        }
+        public ActionResult<Coursier> GetByString(string mail)
+        {
+            return s221UberContext.Coursiers.FirstOrDefault(u => u.EmailUser.ToUpper() == mail.ToUpper());
+        }
+        public void Add(Coursier entity)
+        {
+            s221UberContext.Coursiers.Add(entity);
+            s221UberContext.SaveChanges();
+        }
+        public void Update(Coursier newCoursier, Coursier entity)
+        {
+            s221UberContext.Entry(newCoursier).State = EntityState.Modified;
+            newCoursier.IdCoursier = entity.IdCoursier;
+            newCoursier.IdEntreprise = entity.IdEntreprise;
+            newCoursier.IdAdresse = entity.IdAdresse;
+            newCoursier.GenreUser = entity.GenreUser;
+            newCoursier.NomUser = entity.NomUser;
+            newCoursier.PrenomUser = entity.PrenomUser;
+            newCoursier.DateNaissance = entity.DateNaissance;
+            newCoursier.Telephone = entity.Telephone;
+            newCoursier.EmailUser = entity.EmailUser;
+            newCoursier.MotDePasseUser = entity.MotDePasseUser;
+            newCoursier.PhotoProfile = entity.PhotoProfile;
+            newCoursier.SouhaiteRecevoirBonPlan = entity.SouhaiteRecevoirBonPlan;
+            newCoursier.MfaActivee = entity.MfaActivee;
+            newCoursier.TypeCoursier = entity.TypeCoursier;
+            newCoursier.DemandeSuppression = entity.DemandeSuppression;
+            s221UberContext.SaveChanges();
+        }
+        public void Delete(Coursier utilisateur)
+        {
+            s221UberContext.Coursiers.Remove(utilisateur);
+            s221UberContext.SaveChanges();
+        }
+    }
+}
