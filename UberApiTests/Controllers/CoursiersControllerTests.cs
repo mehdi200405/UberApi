9b9833e4e6b54e4e55aef0f0a47027e8ed7db60b
fadhloum <fadhloum@D252-S32>
2025-03-18 16:22:25 +0100

    ajout du projet de test unitaire

---
+++ UberApiTests/Controllers/CoursiersControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UberApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberApi.Controllers.Tests
+{
+    [TestClass()]
+    public class CoursiersControllerTests
+    {
+        [TestMethod()]
+        public void GetCoursiersAsyncTest()
+        {
+
+        }
+    }
+}
