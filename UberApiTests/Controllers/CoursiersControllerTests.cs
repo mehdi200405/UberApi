aac352659b77b413379470195c713c02f6058d6a
bekhoucm <bekhoucm@D253-S27>
2025-03-25 09:23:17 +0100

    post en bien

--- UberApiTests/Controllers/CoursiersControllerTests.cs
+++ UberApiTests/Controllers/CoursiersControllerTests.cs
@@ -160,46 +160,14 @@ namespace UberApi.Controllers.Tests
 
             // Assert
             Assert.IsNotNull(actionResult);
-            Assert.AreNotSame(coursier, actionResult.Value);
+            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<Coursier>));
+            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult));
+            var result = actionResult.Result as CreatedAtActionResult;
+            Assert.AreEqual(coursier.IdCoursier, ((Coursier) result.Value).IdCoursier);
         }
 
-        [TestMethod]
-        public void PostCouriser_NotValideIdPassed_ReturnsRightItem_AvecMoq()
-        {
-            Coursier coursier = new Coursier
-            {
-                IdCoursier = 1, 
-                IdEntreprise = 1,
-                IdAdresse = 1,
-                GenreUser = "Homme",
-                NomUser = "Amir", 
-                PrenomUser = "Julien",
-                DateNaissance = DateOnly.Parse("1988-04-25"),
-                Telephone = "0601010101",
-                EmailUser = "julien.durant@example.com",
-                MotDePasseUser = "hasedpassword123",
-                NumeroCarteVtc = "123456789012",
-                Iban = "FR7630006000011234567890189",
-                DateDebutActivite = DateOnly.Parse("2023-01-15"),
-                NoteMoyenne = 4.5m,
-                Courses = [],
-                Entretiens = [],
-                Horaires = [],
-                IdAdresseNavigation = null,
-                IdEntrepriseNavigation = null,
-                ReglementSalaires = [],
-                Vehicules = []
-            };
 
-            _mockRepository = new Mock<IDataRepository<Coursier>>();
-            _controller = new CoursiersController(_mockRepository.Object);
-            // Act
-            var actionResult = _controller.PostCoursierAsync(coursier).Result;
-
-            // Assert
-            Assert.IsNotNull(actionResult);
-            Assert.AreSame(coursier.IdCoursier, actionResult.Value.IdCoursier);
-        }
+   
     }
 
 }
