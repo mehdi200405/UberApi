ff3ffa588f49ad9f706a08b1ca2a65365440b36f
bekhoucm <bekhoucm@D253-S27>
2025-03-25 08:34:37 +0100

    post fonctionnel

--- UberApiTests/Controllers/CoursiersControllerTests.cs
+++ UberApiTests/Controllers/CoursiersControllerTests.cs
@@ -125,43 +125,81 @@ namespace UberApi.Controllers.Tests
         }
 
 
-        //[TestMethod]
-        //public void PostCouriser_ExistingIdPassed_ReturnsRightItem_AvecMoq()
-        //{
-        //    Coursier coursier = new Coursier
-        //    {
-        //        IdCoursier = 10, //new coursier id 10
-        //        IdEntreprise = 1,
-        //        IdAdresse = 1,
-        //        GenreUser = "Homme",
-        //        NomUser = "Amir", //new coursier name Amir
-        //        PrenomUser = "Julien",
-        //        DateNaissance = DateOnly.Parse("1988-04-25"),
-        //        Telephone = "0601010101",
-        //        EmailUser = "julien.durant@example.com",
-        //        MotDePasseUser = "hasedpassword123",
-        //        NumeroCarteVtc = "123456789012",
-        //        Iban = "FR7630006000011234567890189",
-        //        DateDebutActivite = DateOnly.Parse("2023-01-15"),
-        //        NoteMoyenne = 4.5m,
-        //        Courses = [],
-        //        Entretiens = [],
-        //        Horaires = [],
-        //        IdAdresseNavigation = null,
-        //        IdEntrepriseNavigation = null,
-        //        ReglementSalaires = [],
-        //        Vehicules = []
-        //    };
+        [TestMethod]
+        public void PostCouriser_ValideIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            Coursier coursier = new Coursier
+            {
+                IdCoursier = 10, //new coursier id 10
+                IdEntreprise = 1,
+                IdAdresse = 1,
+                GenreUser = "Homme",
+                NomUser = "Amir", //new coursier name Amir
+                PrenomUser = "Julien",
+                DateNaissance = DateOnly.Parse("1988-04-25"),
+                Telephone = "0601010101",
+                EmailUser = "julien.durant@example.com",
+                MotDePasseUser = "hasedpassword123",
+                NumeroCarteVtc = "123456789012",
+                Iban = "FR7630006000011234567890189",
+                DateDebutActivite = DateOnly.Parse("2023-01-15"),
+                NoteMoyenne = 4.5m,
+                Courses = [],
+                Entretiens = [],
+                Horaires = [],
+                IdAdresseNavigation = null,
+                IdEntrepriseNavigation = null,
+                ReglementSalaires = [],
+                Vehicules = []
+            };
 
-        //    _mockRepository = new Mock<IDataRepository<Coursier>>();
-        //    _controller = new CoursiersController(_mockRepository.Object);
-        //    // Act
-        //    var actionResult = _controller.PostCoursierAsync(coursier).Result;
+            _mockRepository = new Mock<IDataRepository<Coursier>>();
+            _controller = new CoursiersController(_mockRepository.Object);
+            // Act
+            var actionResult = _controller.PostCoursierAsync(coursier).Result;
 
-        //    // Assert
-        //    Assert.IsNotNull(actionResult);
-        //    Assert.AreSame(coursier, actionResult.Value);
-        //}
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.AreNotSame(coursier, actionResult.Value);
+        }
+
+        [TestMethod]
+        public void PostCouriser_NotValideIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            Coursier coursier = new Coursier
+            {
+                IdCoursier = 1, 
+                IdEntreprise = 1,
+                IdAdresse = 1,
+                GenreUser = "Homme",
+                NomUser = "Amir", 
+                PrenomUser = "Julien",
+                DateNaissance = DateOnly.Parse("1988-04-25"),
+                Telephone = "0601010101",
+                EmailUser = "julien.durant@example.com",
+                MotDePasseUser = "hasedpassword123",
+                NumeroCarteVtc = "123456789012",
+                Iban = "FR7630006000011234567890189",
+                DateDebutActivite = DateOnly.Parse("2023-01-15"),
+                NoteMoyenne = 4.5m,
+                Courses = [],
+                Entretiens = [],
+                Horaires = [],
+                IdAdresseNavigation = null,
+                IdEntrepriseNavigation = null,
+                ReglementSalaires = [],
+                Vehicules = []
+            };
+
+            _mockRepository = new Mock<IDataRepository<Coursier>>();
+            _controller = new CoursiersController(_mockRepository.Object);
+            // Act
+            var actionResult = _controller.PostCoursierAsync(coursier).Result;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.AreSame(coursier.IdCoursier, actionResult.Value.IdCoursier);
+        }
     }
 
 }
