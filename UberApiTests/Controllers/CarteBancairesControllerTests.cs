c8e8ed88eddcd56fbfb88c1e5cb5286d223d1f2c
AmirBekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2025-04-10 02:20:23 +0200

    GROS CACA

--- UberApiTests/Controllers/CarteBancairesControllerTests.cs
+++ UberApiTests/Controllers/CarteBancairesControllerTests.cs
@@ -137,45 +137,7 @@ namespace UberApi.Controllers.Tests
         }
 
 
-        [TestMethod]
-        public void PostCarteBancaire_ValideIdPassed_ReturnsRightItem_AvecMoq()
-        {
-            // Arrange
-            var carteBancaire = new CarteBancaire
-            {
-                IdCb = 10,
-                NumeroCb = "1234567812345679",
-                DateExpireCb = DateOnly.Parse("2025-12-31"),
-                Cryptogramme = "123",
-                TypeCarte = "Crédit",
-                TypeReseaux = "MasterCard",
-                Courses = [],
-                IdClients = []
-            };
-
-            var mockRepository = new Mock<ICarteBancaireRepository>();
-
-            mockRepository.Setup(x => x.AddAsync(It.IsAny<CarteBancaire>())).Returns(Task.CompletedTask);
-            mockRepository.Setup(x => x.GetByIdAsync(It.Is<int>(id => id == carteBancaire.IdCb)))
-                           .ReturnsAsync(carteBancaire);
-
-            var controller = new CarteBancairesController(mockRepository.Object);
-
-            // Act
-            var idClient = 20;
-            var actionResult = controller.PostCarteBancaireAsync(carteBancaire, idClient).Result;
-
-            // Assert
-            Assert.IsNotNull(actionResult);
-            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<CarteBancaire>));
-            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult));
-            var result = actionResult.Result as CreatedAtActionResult;
-            Assert.IsNotNull(result);
-            var ress = result.Value as CarteBancaire;
-            Assert.IsNotNull(ress);
-            Assert.AreEqual(carteBancaire.IdCb, ress.IdCb);
-            mockRepository.Verify(x => x.AddAsync(It.IsAny<CarteBancaire>()), Times.Once);
-        }
+      
 
 
         [TestMethod]
@@ -253,18 +215,7 @@ namespace UberApi.Controllers.Tests
         /// SANS MOQ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /// </summary>
 
-        [TestMethod()]
-        public void GetCarteBancaires_SansMoq()
-        {
 
-            List<CarteBancaire> expected = _context.CarteBancaires.ToList();
-
-
-            // Act
-            var actionResult = _controller.GetCarteBancairesAsync().Result;
-            // Assert
-            CollectionAssert.AreEqual(expected, actionResult.Value.ToList(), "");
-        }
 
         [TestMethod()]
         public void GetCarteBancaireById_ExistingIdPassedOrNot_AreEqual_SansMoq()
@@ -315,71 +266,19 @@ namespace UberApi.Controllers.Tests
         }
 
 
-        [TestMethod]
-        public void PostCarteBancaire_ValideIdPassed_ReturnsRightItem_SansMoq()
-        {
-            // Arrange
-            Random rnd = new Random();
-            string chiffreCarteNew = "";
-            for (int i = 0; i < 16; i++)
-            {
-                chiffreCarteNew += rnd.Next(1, 9);
-            }
 
-
-
-
-            // Le mail doit être unique donc 2 possibilités :
-            // 1. on s'arrange pour que le mail soit unique en concaténant un random ou un timestamp
-            // 2. On supprime le user après l'avoir créé. Dans ce cas, nous avons besoin d'appeler la méthode DELETE de l’API ou remove du DbSet.
-            CarteBancaire cousierATester = new CarteBancaire()
-            {
-
-                NumeroCb = chiffreCarteNew,
-                DateExpireCb = DateOnly.Parse("2025-12-31"),
-                Cryptogramme = "123",
-                TypeCarte = "Crédit",
-                TypeReseaux = "MasterCard",
-                Courses = [],
-                IdClients = []
-            };
-            // Act
-            try
-            {
-                var idClient = 20;
-                var result = _controller.PostCarteBancaireAsync(cousierATester, idClient).Result;
-            }
-            catch (AggregateException ex)
-            {
-                foreach (var inner in ex.InnerExceptions)
-                {
-                    Console.WriteLine($"Inner Exception: {inner.Message}");
-                    if (inner.InnerException != null)
-                        Console.WriteLine($"Detailed Inner Exception: {inner.InnerException.Message}");
-                }
-                throw;
-            } // .Result pour appeler la méthode async de manière synchrone, afin d'attendre l’ajout
-            // Assert
-            CarteBancaire? userRecupere = _context.CarteBancaires.Where(u => u.NumeroCb.ToUpper() ==
-            cousierATester.NumeroCb.ToUpper()).FirstOrDefault(); // On récupère l'utilisateur créé directement dans la BD grace à son mail unique
-            // On ne connait pas l'ID de l’utilisateur envoyé car numéro automatique.
-            // Du coup, on récupère l'ID de celui récupéré et on compare ensuite les 2 users
-            cousierATester.IdCb = userRecupere.IdCb;
-            Assert.AreEqual(userRecupere, cousierATester, "cb pas identiques");
-
-        }
 
 
 
         [TestMethod]
-        public void DeleteCarteBancaire_ValideIdPassed_ReturnsNoContent_SansMoq()
+        public void DeleteCarteBancaire_WhenValidIdPassed_ShouldReturnNoContent()
         {
 
             Random rnd = new Random();
             string chiffreCarteNew = "";
             for (int i = 0; i < 16; i++)
             {
-                chiffreCarteNew += rnd.Next(1, 9);
+                chiffreCarteNew += rnd.Next(0, 10);
             }
 
 
@@ -397,15 +296,13 @@ namespace UberApi.Controllers.Tests
             };
             var idClient = 1;
             var result = _controller.PostCarteBancaireAsync(cousierATester, idClient).Result;
-            var ress = result.Result as ActionResult;
+
             // Act : Suppression
-            var actionResult = _controller.DeleteCarteBancaireAsync(ress.Value).Result;
+            var actionResult = _controller.DeleteCarteBancaireAsync(cousierATester.IdCb).Result;
 
             // Assert : Vérification de la réponse
             Assert.IsNotNull(actionResult);
             Assert.IsInstanceOfType(actionResult, typeof(NoContentResult));
-
-
 
 
         }
